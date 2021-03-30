bbffea7293953ebd39f2632a16611beb4456b055
UnicornCodeQueen <75204601+UnicornCodeQueen@users.noreply.github.com>
2021-03-30 17:18:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+document.getElementById("Jamboard_button").onclick = function () {
+    location.href = "https://jamboard.google.com/d/1NNwHidu7ugRyeoGfYLZRSRakutredFyhEYzKLc2UXiU/edit?usp=sharing";
+}
